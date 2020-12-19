ab149733db69bec682cddef964294d44f50b84e0
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2020-12-19 10:56:25 -0500

    Move manual testing

--- index.js
+++ index.js
@@ -1,46 +1,4 @@
 const css = require('./src/css.js');
 const html = require('./src/html.js');
 
-// To be atomized
-const styles = `
-.cow,
-.cat {
-    font-size: 12px;
-    padding: 8px;
-}
-.dog {
-    font-size: 12px;
-    background: #F00;
-    padding: 8px;
-}`;
 
-// Original classes ot be replaced with atomized versions
-const markup = `<!DOCTYPE html>
-<html>
-  <head>
-    <title>Test</title>
-  </head>
-  <body>
-    <p class="cool cow moo">
-      Hi there!
-    </p>
-    <!--
-      <span class="dog">comments are skipped</span>
-    -->
-    <h1 class="cool cat nice wow">
-      Meow
-    </h1>
-    <h2 class="dog">
-      Woof
-    </h2>
-  </body>
-</html>`;
-
-
-
-const processedStyles = css(styles);
-const updatedMarkup = html(markup, processedStyles);
-
-console.log(processedStyles.classMap);
-console.log(processedStyles.output);
-console.log(updatedMarkup);
