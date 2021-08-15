c83d5d66b2c6beca201b80063e67b163eb7065f4
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2021-08-15 16:40:19 -0400

    Break up main function

--- index.js
+++ index.js
@@ -8,96 +8,119 @@ const redPerfume = {
   validate: function (options) {
     return validator.validateOptions(options);
   },
+  getCssString: function (options, taskStyles) {
+    let cssString = '';
+    let styleErrors = [];
+
+    if (taskStyles.in) {
+      taskStyles.in.forEach((file) => {
+        try {
+          cssString = cssString + String(fs.readFileSync(file));
+        } catch (err) {
+          helpers.throwError(options, 'Error reading style file: ' + file, err);
+          styleErrors.push(err);
+        }
+      });
+    }
+    if (taskStyles.data) {
+      cssString = cssString + taskStyles.data;
+    }
+
+    return { cssString, styleErrors };
+  },
+  getHtmlString: function (options, item) {
+    let markupString = '';
+    let markupErrors = [];
+    if (item.in) {
+      try {
+        markupString = markupString + String(fs.readFileSync(item.in));
+      } catch (err) {
+        helpers.throwError(options, 'Error reading markup file: ' + item.in, err);
+        markupErrors.push(err);
+      }
+    }
+    if (item.data) {
+      markupString = markupString + item.data;
+    }
+    return { markupString, markupErrors };
+  },
+  outputAtomizedCSS: function (options, taskStyles, processedStyles, styleErrors) {
+    if (taskStyles.out) {
+      try {
+        fs.writeFileSync(taskStyles.out, processedStyles.output + '\n');
+      } catch (err) {
+        helpers.throwError(options, 'Error writing CSS file: ' + taskStyles.out, err);
+        styleErrors.push(err);
+      }
+    }
+    if (taskStyles.result) {
+      if (!styleErrors.length) {
+        styleErrors = undefined;
+      }
+      taskStyles.result(processedStyles.output, styleErrors);
+    }
+  },
+  outputAtomizedHTML: function (options, item, processedMarkup, markupErrors) {
+    if (item.out) {
+      try {
+        fs.writeFileSync(item.out, processedMarkup + '\n');
+      } catch (err) {
+        helpers.throwError(options, 'Error writing markup file: ' + item.out, err);
+        markupErrors.push(err);
+      }
+    }
+    if (item.result) {
+      if (!markupErrors.length) {
+        markupErrors = undefined;
+      }
+      item.result(processedMarkup, markupErrors);
+    }
+  },
+  outputAtomizedJSON: function (options, taskScripts, processedStyles) {
+    let scriptErrors;
+    if (taskScripts.out) {
+      try {
+        fs.writeFileSync(taskScripts.out, JSON.stringify(processedStyles.classMap, null, 2) + '\n');
+      } catch (scriptErr) {
+        helpers.throwError(options, 'Error writing script file: ' + taskScripts.out, scriptErr);
+        scriptErrors = scriptErr;
+      }
+    }
+    if (taskScripts.result) {
+      taskScripts.result(processedStyles.classMap, scriptErrors);
+    }
+  },
   atomize: function (options) {
     options = this.validate(options);
     options.tasks.forEach((task) => {
       let processedStyles = {};
+
       if (task.styles) {
-        let styleData = '';
-        let styleErrors = [];
-        if (task.styles.in) {
-          task.styles.in.forEach((file) => {
-            try {
-              styleData = styleData + String(fs.readFileSync(file));
-            } catch (err) {
-              helpers.throwError(options, 'Error reading style file: ' + file, err);
-              styleErrors.push(err);
-            }
-          });
+        const cssData = this.getCssString(options, task.styles);
+
+        const hasStyleFiles = task.styles.in && task.styles.in.length;
+        if (hasStyleFiles || task.styles.data) {
+          processedStyles = css(options, cssData.cssString, task.uglify);
         }
-        if (task.styles.data) {
-          styleData = styleData + task.styles.data;
-        }
-        if (
-          (task.styles.in && task.styles.in.length) ||
-          task.styles.data
-        ) {
-          processedStyles = css(options, styleData, task.uglify);
-        }
-        if (task.styles.out) {
-          try {
-            fs.writeFileSync(task.styles.out, processedStyles.output + '\n');
-          } catch (err) {
-            helpers.throwError(options, 'Error writing CSS file: ' + task.styles.out, err);
-            styleErrors.push(err);
-          }
-        }
-        if (task.styles.result) {
-          if (!styleErrors.length) {
-            styleErrors = undefined;
-          }
-          task.styles.result(processedStyles.output, styleErrors);
-        }
+
+        this.outputAtomizedCSS(options, task.styles, processedStyles, cssData.styleErrors);
       }
+
       if (task.markup) {
         task.markup.forEach((item) => {
-          let processedMarkup;
-          let markupData = '';
-          let markupErrors = [];
+          const htmlData = this.getHtmlString(options, item);
 
-          if (item.in) {
-            try {
-              markupData = markupData + String(fs.readFileSync(item.in));
-            } catch (err) {
-              helpers.throwError(options, 'Error reading markup file: ' + item.in, err);
-              markupErrors.push(err);
-            }
-          }
-          if (item.data) {
-            markupData = markupData + item.data;
-          }
+          let processedMarkup = '';
           if (item.in || item.data) {
-            processedMarkup = html(options, markupData, processedStyles.classMap);
+            processedMarkup = html(options, htmlData.markupString, processedStyles.classMap);
           }
-          if (item.out) {
-            try {
-              fs.writeFileSync(item.out, processedMarkup + '\n');
-            } catch (err) {
-              helpers.throwError(options, 'Error writing markup file: ' + item.out, err);
-              markupErrors.push(err);
-            }
-          }
-          if (item.result) {
-            if (!markupErrors.length) {
-              markupErrors = undefined;
-            }
-            item.result(processedMarkup, markupErrors);
-          }
+
+          this.outputAtomizedHTML(options, item, processedMarkup, htmlData.markupErrors);
         });
       }
+
       if (task.scripts) {
-        let scriptErrors;
-        if (task.scripts.out) {
-          try {
-            fs.writeFileSync(task.scripts.out, JSON.stringify(processedStyles.classMap, null, 2) + '\n');
-          } catch (scriptErr) {
-            helpers.throwError(options, 'Error writing script file: ' + task.scripts.out, scriptErr);
-            scriptErrors = scriptErr;
-          }
-        }
-        if (task.scripts.result) {
-          task.scripts.result(processedStyles.classMap, scriptErrors);
-        }
+        this.outputAtomizedJSON(options, task.scripts, processedStyles);
       }
     });
   }
