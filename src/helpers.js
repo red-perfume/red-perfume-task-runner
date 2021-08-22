e1175caa50b768797c243c9b4d8b47d7ed472625
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2021-08-21 23:56:45 -0400

    Improve logging helper

--- src/helpers.js
+++ src/helpers.js
@@ -7,6 +7,30 @@
 
 const helpers = {
   /**
+   * Takes a string and replaces spaces with returns if
+   * they are the last space on a console line (108 chars).
+   *
+   * @param  {string} message  Human readable warning/error
+   * @return {string}          Message with returns added
+   */
+  insertReturns: function (message) {
+    const maxLineLength = 108;
+    let words = message.split(' ');
+    let line = '';
+    let lines = [];
+    words.forEach(function (word) {
+      let potentialLine = line + ' ' + word;
+      if (potentialLine.length < maxLineLength) {
+        line = potentialLine;
+      } else {
+        lines.push(line);
+        line = word;
+      }
+    });
+    lines.push(line.trim());
+    return lines.join('\n').trim();
+  },
+  /**
    * Either calls the customLogger or does
    * console.error when errors/warnings happen
    * during validation or execution.
@@ -18,12 +42,18 @@ const helpers = {
   throwError: function (options, message, error) {
     if (options.verbose && options.customLogger) {
       options.customLogger(message, error);
-    } else if (options.verbose) {
+    } else if (options.verbose && options.error) {
       console.error(
         '_________________________\n' +
         'Red-Perfume:\n' +
-        message,
+        this.insertReturns(message),
         error
+      );
+    } else if (options.verbose) {
+      console.warn(
+        '_________________________\n' +
+        'Red-Perfume:\n' +
+        this.insertReturns(message)
       );
     }
   }
